5875803eed6a0a683611453d0c150ee2cbc6d02b
xisoDev <xiso@xiso.co.kr>
2016-11-07 12:18:47 +0900

    커밋한다

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -34,10 +34,63 @@ angular.module('xisodip.controllers', [])
 })
 
 
-.controller('sequenceCtrl', function($scope, $stateParams, Chats) {
+.controller('sequenceCtrl', function($scope, $ionicModal, sequences) {
+  $scope.sequences = sequences.all();
+  $scope.remove = function(sequence) {
+    sequences.remove(sequence);
+  };
+
+  $ionicModal.fromTemplateUrl('sequence-add.html', {
+    scope: $scope,
+    animation: 'slide-in-up'
+  }).then(function(modal) {
+    $scope.sequence_add = modal;
+  });
+
+  $scope.addSequence = function(){
+    $scope.sequence_add.show();
+  }
+
+  $scope.closeAddDevice = function() {
+    $scope.sequence_add.hide();
+  };
 })
 
-.controller('sequenceDetailCtrl', function($scope, $stateParams, Chats) {
+.controller('sequenceDetailCtrl', function($scope, $stateParams,sequences,$ionicActionSheet, $timeout) {
+  $scope.sequence = sequences.get($stateParams.sequenceSrl);
+
+  $scope.addClip = function() {
+    // Show the action sheet
+    var hideSheet = $ionicActionSheet.show({
+      buttons: [
+        { text: '새로운 이미지 클립 업로드' },
+        { text: '새로운 동영상 클립 업로드' },
+        { text: '서버에 업로드된 클립 재사용' }
+      ],
+      titleText: '클립을 업로드하거나 재사용합니다.',
+      cancelText: '취소',
+      cancel: function() {
+        // add cancel code..
+      },
+      buttonClicked: function(index) {
+        return true;
+      }
+    });
+
+    // For example's sake, hide the sheet after two seconds
+    $timeout(function() {
+      hideSheet();
+    }, 4000);
+
+  };
+
+  $scope.clipReorder = function(play_list, fromIndex, toIndex) {
+    console.log("순서정렬 : " + fromIndex + "->" + toIndex);
+    var temp = $scope.sequence.play_list[toIndex];
+    $scope.sequence.play_list[toIndex] = $scope.sequence.play_list[fromIndex];
+    $scope.sequence.play_list[fromIndex] = temp;
+  };
+
 })
 
 .controller('configCtrl', function($scope) {
--- www/js/services.js
+++ www/js/services.js
@@ -8,6 +8,7 @@ angular.module('xisodip.services', [])
     srl: 0,
     name: 'Samsung Galaxy Note 10.1',
     sequence: '테스트용 시퀀스1',
+    sequence_srl: 1,
     content: '부경대학교 정문 주차장옆에 설치된 갤럭시 노트 10.1',
     image: 'img/note101.jpg'
   }, {
@@ -15,17 +16,20 @@ angular.module('xisodip.services', [])
     name: 'LG GPAD 7.0',
     content: '사관학교 엘리베이터 내부 광고용 디스플레이',
     sequence: '테스트용 시퀀스2',
+    sequence_srl: 2,
     image: 'img/gpad.jpg'
   }, {
     srl: 2,
     name: 'LG GPAD 7.0',
     content: '본관 1층 광고용 디스플레이',
     sequence: '테스트용 시퀀스1',
+    sequence_srl: 1,
     image: 'img/gpad.jpg'
   }, {
     srl: 3,
     name: 'Samsung Galaxy Note 10.1',
     sequence: '테스트용 시퀀스3',
+    sequence_srl: 3,
     content: '후문 옆 버스정류장 (건너편)에 설치한 디스플레이',
     image: 'img/note101.jpg'
   }];
@@ -41,6 +45,93 @@ angular.module('xisodip.services', [])
       for (var i = 0; i < devices.length; i++) {
         if (devices[i].srl === parseInt(deviceSrl)) {
           return devices[i];
+        }
+      }
+      return null;
+    }
+  };
+})
+.factory('sequences', function() {
+  // Might use a resource here that returns a JSON array
+
+  // Some fake testing data
+  var sequences = [{
+    srl: 0,
+    title: 'XISO 광고용 시퀀스',
+    main_image: 'sequence/dgdgdgdg.jpg',
+    running_time: '119분 11초',
+    play_list: {
+      0: {
+        title: "타이포그라피 광고",
+        type: "Image/JPEG",
+        time: "15초",
+        transition: "slide-up",
+        thumbnail: "sequence/1.jpg"
+      },
+      1: {
+        title: "영화패러디",
+        type: "Image/GIF",
+        time: "15초",
+        transition: "slide-left",
+        thumbnail: "sequence/2.jpg"
+      },
+      2: {
+        title: "영화패러디 2",
+        type: "Image/JPEG",
+        time: "15초",
+        transition: "fade-out",
+        thumbnail: "sequence/3.jpg"
+      },
+      3: {
+        title: "VFX 광고",
+        type: "Image/PNG",
+        time: "15초",
+        transition: "slide-up",
+        thumbnail: "sequence/4.jpg"
+      },
+      4: {
+        title: "타이포그라피 광고",
+        type: "Video/MP4",
+        time: "15초",
+        transition: "slide-up",
+        thumbnail: "sequence/5.jpg"
+      },
+      5: {
+        title: "타이포그라피 광고",
+        type: "Video/MP4",
+        time: "15초",
+        transition: "slide-up",
+        thumbnail: "sequence/6.jpg"
+      },
+      6: {
+        title: "타이포그라피 광고",
+        type: "Video/MP4",
+        time: "15초",
+        transition: "slide-up",
+        thumbnail: "sequence/7.png"
+      },
+      7: {
+        title: "타이포그라피 광고",
+        type: "Video/MP4",
+        time: "15초",
+        transition: "slide-up",
+        thumbnail: "sequence/8.jpg"
+      }
+    },
+    text_clip: '공을 오늘도 수치감에얼굴이 싶은대로 때리자, 하고는 가자,리나!아,아 때 혼나기 옷을 화원 아들의 리나가 덜아가시고나서 목소리가 자지를 하지는 두영이가 생각을 문지르자 흥분을 과일 하면서    계곡이 못하고 안으로 얼굴이 쪼그리고 안입어서 들린다엄마, 먹으면 없는 만들어 차려져 무릎을 하나 난다엄마가 나와서 말 앞으로 빨기시작했다다시 받지 진숙은 내 하고 나고 해알았어,,리'
+  }];
+
+  return {
+    all: function() {
+      return sequences;
+    },
+    remove: function(sequence) {
+      sequences.splice(sequences.indexOf(sequence), 1);
+    },
+    get: function(sequenceSrl) {
+      for (var i = 0; i < sequences.length; i++) {
+        if (sequences[i].srl === parseInt(sequenceSrl)) {
+          return sequences[i];
         }
       }
       return null;
